4deb222506df508c40325bcf8b3a5b0c94dd570e
alexandrawallace <alexandrawallace367@gmail.com>
2023-11-27 00:29:13 +0800

    update

--- themes/fukasawa/components/TopNav.js
+++ themes/fukasawa/components/TopNav.js
@@ -21,8 +21,8 @@ const TopNav = props => {
   return (<div id='top-nav' className='z-40 block lg:hidden'>
 
     {/* 导航栏 */}
-    <DarkModeButton />
     <div id='sticky-nav' className={'relative w-full top-0 z-20 transform duration-500 bg-white dark:bg-black'}>
+      <DarkModeButton />
       <Collapse type='vertical' isOpen={isOpen} collapseRef={collapseRef}>
         <div className='py-1 px-5'>
           <MenuList {...props} onHeightChange={(param) => collapseRef.current?.updateCollapseHeight(param)} />
